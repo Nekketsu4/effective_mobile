e65e1c3efbc070b82c16afe413d04e124300779f
Kadyr <Nekketsu4@mail.ru>
2026-03-28 14:11:27 +0300

    feat: added role_repo.py

---
+++ app/repositories/role_repo.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.models.role import Role
+from app.repositories.base_repo import BaseRepository
+
+
+class RoleRepository(BaseRepository):
+    model = Role
+
+    async def get_by_name(self, name: str) -> Role | None:
+        role = await self.db.execute(select(Role).where(Role.name == name))
+        return role.scalar_one_or_none()
+
+    async def get_all(self):
+        roles = await self.db.execute(select(Role))
+        return list(roles.scalars().all())
