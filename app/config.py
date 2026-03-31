11bbed9455a20e1b8d0efc72f7991dfe03a2547c
Kadyr <Nekketsu4@mail.ru>
2026-03-31 13:03:07 +0300

    feat: added config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
 
-    DATABASE_URL: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
     DATABASE_URL_TEST: str
 
     JWT_SECRET: str
